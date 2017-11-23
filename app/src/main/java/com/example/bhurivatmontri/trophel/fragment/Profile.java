bdc1ee93f1e60f112e9d7499d5ec822ef0351f9c
Bhurivat Montri <auntafung@hotmail.com>
2017-11-23 12:37:04 +0700

    lastest

--- app/src/main/java/com/example/bhurivatmontri/trophel/fragment/Profile.java
+++ app/src/main/java/com/example/bhurivatmontri/trophel/fragment/Profile.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.bhurivatmontri.trophel.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -15,6 +21,8 @@ import com.example.bhurivatmontri.trophel.R;
  */
 public class Profile extends Fragment {
 
+    protected DatabaseReference mDatabase;
+    protected TextView idUser;
 
     public Profile() {
         // Required empty public constructor
@@ -25,7 +33,26 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        idUser = (TextView) view.findViewById(R.id.id_Profile);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("users").child("uID").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.child("id").getValue().toString();;
+                idUser.setText("id:"+value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
+
     }
 
 }
